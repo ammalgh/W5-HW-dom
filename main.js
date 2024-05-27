d5bb0ab31258e3f85415f2d81e116bc63a97ea85
ammalgh <ammalgh22@gmail.com>
2024-05-27 15:29:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function showHoverContent() {
+    document.getElementById('default-content').style.display = 'none';
+    document.getElementById('hover-content').style.display = 'block';
+}
+
+function showDefaultContent() {
+    document.getElementById('default-content').style.display = 'block';
+    document.getElementById('hover-content').style.display = 'none';
+}
+
+
+// function showHoverContent(section) {
+//     document.getElementById(`${section}-default`).hidden = true;
+//     document.getElementById(`${section}-hover`).hidden = false;
+// }
+
+// function showDefaultContent(section) {
+//     document.getElementById(`${section}-default`).hidden = false;
+//     document.getElementById(`${section}-hover`).hidden = true;
+// }
+
+
+
